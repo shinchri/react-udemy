e7d6d07073766fe90526dd23ee68168b0dd1a04d
Chris Shin <17078706+shinchri@users.noreply.github.com>
2022-01-04 11:25:08 -0600

    chore: Refactoring

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import './ExpenseItem.css';
+import ExpenseDate from './ExpenseDate'
+
+function ExpenseItem(props) {
+    const { title, amount, date } = props
+
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={date} />
+            <div className="expense-item__description">
+                <h2>{title}</h2>
+                <div className="expense-item__price">${amount}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
